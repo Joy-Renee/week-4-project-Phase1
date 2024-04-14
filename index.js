12a7506398a614ec23185cce38411e38bc3688ab
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-14 16:49:37 +0300

    completed

--- index.js
+++ index.js
@@ -1,28 +1,5 @@
 let roomBooking = document.querySelector('#roomBooking')
 
-const forTitles = function() {
-    fetch('http://localhost:3000/rooms')
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
-        data.forEach(room => {
-            //console.log(room);
-            const titleList = document.createElement('ul')
-            titleList.innerHTML = `
-            <ul class ='btn' id='${room.id}' > ${room.title}</ul> 
-            `
-            // ul-btn.style.backgroundColor = 'yellow'
-            roomBooking.appendChild(titleList)
-
-
-        })
-    })
-    
-}
-
-forTitles()
-
-
 
 function bookRooms(){
     fetch('http://localhost:3000/rooms')
@@ -56,15 +33,24 @@ function bookRooms(){
 
 bookRooms()
 
-// roomBooking.innerHTML = `
-// <div class="roomDetails">
-//     <div class="picture">
-//         ${room.picture}
-//     </div>
-//     <div class="details">
-//     ${room.title}
-//     ${room.capacity}
-//     ${room.description}
-//     </div>
-// </div>
-//`
+
+// const forTitles = function() {
+//     fetch('http://localhost:3000/rooms')
+//     .then(res => res.json())
+//     .then(data => {
+//         console.log(data);
+//         data.forEach(room => {
+//             //console.log(room);
+//             const titleList = document.createElement('ul')
+//             titleList.innerHTML = `
+//             <ul class ='btn' id='${room.id}' > ${room.title}</ul> 
+//             `
+//             roomBooking.appendChild(titleList)
+
+
+//         })
+//     })
+    
+// }
+
+// forTitles()
