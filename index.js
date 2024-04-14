4241f7f6daa8989e6fddfb5caa3970b80625467c
Joy Renee Mugo <mugojoy24@gmail.com>
2024-04-14 13:40:30 +0300

    fiftenth commit

--- index.js
+++ index.js
@@ -36,14 +36,11 @@ function bookRooms(){
                 
                 roomBooking.innerHTML = `
                 <div class="roomDetails">
-                    <div class="picture">
-                     ${room.picture}
-                    </div>
                     <div class="details">
-                   <div class='title'> ${room.title} </div>
-                   <div class='capacity'> ${room.capacity} people </div>
-                   <div class='description'> ${room.description} </div>
-                   <div class='price'> ${room.price} $</div>
+                        <div class='title'> ${room.title} </div>
+                        <div class='capacity'> ${room.capacity} people </div>
+                        <div class='description'> ${room.description} </div>
+                        <div class='price'> ${room.price} $</div>
                     </div>
                 </div>
                 `
